30ddc8c64d78d74a42702d7638983dcc0bbe43ad
Mochamad Hafidh Dwyanto <73733715+fidzer29@users.noreply.github.com>
2022-06-09 22:01:35 +0700

    Add files via upload

---
+++ Proyek 2 - VUE - Teks Editor/main.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <windows.h>
+#include "teksEditor.H"
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char** argv) {
+	
+	tampilan_awal();
+	editorKeyProses();
+	
+	return 0;
+}
